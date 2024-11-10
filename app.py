8279c97ec27e51d2e766a9e9603f3650f095cf04
Heinrich <heinrich.delange@gmail.com>
2024-11-10 08:55:55 +0200

    Added support for multiple chargers

--- app.py
+++ app.py
@@ -29,17 +29,33 @@ mqtt_user = config['mqtt_user']
 mqtt_password = config['mqtt_password']
 scan_interval = config['scan_interval']
 ha_discovery_enabled = config['mqtt_ha_discovery']
+module_address_list = config['module_address']
 
 # Initialize the UXRChargerModule
 module = UXRChargerModule(channel=config['port'])
-address = 0x03
+
+# address = 0x03
 group = 0x05
 
-# Static parameters to read once
-rated_power = module.get_rated_output_power(address, group)
-rated_current = module.get_rated_output_current(address, group)
-print(f"Rated Output Power: {rated_power} W")
-print(f"Rated Output Current: {rated_current} A")
+uxr_modules = []
+
+# Loop through each address in the list and create an entry in the devices dictionary
+for address in module_address_list:
+    time.sleep(0.1)
+    rated_power = module.get_rated_output_power(address, group)
+    time.sleep(0.1)
+    rated_current = module.get_rated_output_current(address, group)
+    time.sleep(0.1)
+    serial_no = str(module.get_serial_number(address, group))
+    uxr_modules[address] = {
+        "rated_power": rated_power,
+        "rated_current": rated_current,
+        "serial_no": serial_no
+    }
+    print(f"Address: {address} ")
+    print(f"Rated Output Power: {rated_power} W")
+    print(f"Rated Output Current: {rated_current} A")
+    print(f"Serial No: {serial_no}")
 
 # MQTT Callbacks
 mqtt_connected = False
@@ -50,12 +66,14 @@ def on_connect(client, userdata, flags, rc):
     global mqtt_connected
     print("Connected to MQTT broker")
     mqtt_connected = True
-    client.subscribe([
-        (f"{MQTT_BASE_TOPIC}/set/group_id", 0),
-        (f"{MQTT_BASE_TOPIC}/set/output_voltage", 0),
-        (f"{MQTT_BASE_TOPIC}/set/current_limit", 0),
-        (f"{MQTT_BASE_TOPIC}/set/current", 0)
-    ])
+    for address in uxr_modules:
+        serial_no = uxr_modules[address]['serial_no']
+        client.subscribe([
+            (f"{MQTT_BASE_TOPIC}/{serial_no}/set/group_id", 0),
+            (f"{MQTT_BASE_TOPIC}/{serial_no}/set/output_voltage", 0),
+            (f"{MQTT_BASE_TOPIC}/{serial_no}/set/current_limit", 0),
+            (f"{MQTT_BASE_TOPIC}/{serial_no}/set/current", 0)
+        ])
 
 def on_disconnect(client, userdata, flags, rc):
     global mqtt_connected
@@ -67,18 +85,20 @@ def on_message(client, userdata, msg):
     with lock:
         topic = msg.topic
         payload = float(msg.payload.decode())
-        if topic == f"{MQTT_BASE_TOPIC}/set/altitude":
-            module.set_altitude(payload, address, group)
-        elif topic == f"{MQTT_BASE_TOPIC}/set/group_id":
-            module.set_group_id(int(payload), address)
-        elif topic == f"{MQTT_BASE_TOPIC}/set/output_voltage":
-            module.set_output_voltage(payload, address, group)
-        elif topic == f"{MQTT_BASE_TOPIC}/set/current_limit":
-            percentage = payload / rated_current
-            print("Current limit set: {}%".format(percentage))
-            module.set_current_limit(percentage, address, group)
-        elif topic == f"{MQTT_BASE_TOPIC}/set/current":
-            module.set_output_current(payload, address, group)
+        for address in uxr_modules:
+            serial_no = uxr_modules[address]['serial_no']
+            if topic == f"{MQTT_BASE_TOPIC}/{serial_no}/set/altitude":
+                module.set_altitude(payload, address, group)
+            elif topic == f"{MQTT_BASE_TOPIC}/{serial_no}/set/group_id":
+                module.set_group_id(int(payload), address)
+            elif topic == f"{MQTT_BASE_TOPIC}/{serial_no}/set/output_voltage":
+                module.set_output_voltage(payload, address, group)
+            elif topic == f"{MQTT_BASE_TOPIC}/{serial_no}/set/current_limit":
+                percentage = payload / rated_current
+                print("Current limit set: {} for {}%".format(percentage, serial_no))
+                module.set_current_limit(percentage, address, group)
+            elif topic == f"{MQTT_BASE_TOPIC}/{serial_no}/set/current":
+                module.set_output_current(payload, address, group)
 
 # Initialize MQTT client
 client = mqtt.Client()
@@ -92,13 +112,17 @@ client.loop_start()
 # Clean up on exit
 def exit_handler():
     print("Script exiting")
-    client.publish(f"{MQTT_BASE_TOPIC}/availability", "offline", retain=True)
+    for address in uxr_modules:
+        serial_no = uxr_modules[address]['serial_no']
+        client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/availability", "offline", retain=True)
     client.loop_stop()
 
 atexit.register(exit_handler)
 
 # HA Discovery Function
-def ha_discovery():
+def ha_discovery(address):
+    uxr_module = uxr_modules[address]
+    serial_no = uxr_module['serial_no']
     if ha_discovery_enabled:
         print("Publishing HA Discovery topics...")
         # Define device information
@@ -106,11 +130,11 @@ def ha_discovery():
             "manufacturer": "UXR",
             "model": "ChargerModule",
             "identifiers": ["uxr_charger"],
-            "name": "UXR Charger"
+            "name": f"UXR Charger {serial_no}"
         }
 
         # Base availability topic
-        availability_topic = f"{MQTT_BASE_TOPIC}/availability"
+        availability_topic = f"{MQTT_BASE_TOPIC}/{serial_no}/availability"
 
         # Define all sensor parameters and publish discovery messages
         parameters = {
@@ -137,29 +161,29 @@ def ha_discovery():
         for param, details in parameters.items():
             discovery_payload = {
                 "name": param,
-                "unique_id": f"uxr_{param.replace(' ', '_').lower()}",
-                "state_topic": f"{MQTT_BASE_TOPIC}/{param.replace(' ', '_').lower()}",
+                "unique_id": f"uxr_{serial_no}_{param.replace(' ', '_').lower()}",
+                "state_topic": f"{MQTT_BASE_TOPIC}/{serial_no}/{param.replace(' ', '_').lower()}",
                 "availability_topic": availability_topic,
                 "device": device,
                 "device_class": details.get("device_class"),
                 "unit_of_measurement": details.get("unit"),
             }
-            discovery_topic = f"{MQTT_HA_DISCOVERY_TOPIC}/sensor/uxr/{param.replace(' ', '_').lower()}/config"
+            discovery_topic = f"{MQTT_HA_DISCOVERY_TOPIC}/sensor/uxr/{serial_no}/{param.replace(' ', '_').lower()}/config"
             client.publish(discovery_topic, json.dumps(discovery_payload), retain=True)
 
         # Define settable parameters as MQTT number entities
         settable_parameters = {
-            "Current Limit": {"min": 0, "max": rated_current, "step": 0.1, "unit": "A", "command_topic": f"{MQTT_BASE_TOPIC}/set/current_limit"},
-            "Output Voltage": {"min": 0, "max": 500, "step": 0.1, "unit": "V", "command_topic": f"{MQTT_BASE_TOPIC}/set/output_voltage"},
-            "Output Current": {"min": 0, "max": rated_current, "step": 0.1, "unit": "A", "command_topic": f"{MQTT_BASE_TOPIC}/set/current"},
-            "Altitude": {"min": 1000, "max": 5000, "step": 100, "unit": "m", "command_topic": f"{MQTT_BASE_TOPIC}/set/altitude"}
+            "Current Limit": {"min": 0, "max": rated_current, "step": 0.1, "unit": "A", "command_topic": f"{MQTT_BASE_TOPIC}/{serial_no}/set/current_limit"},
+            "Output Voltage": {"min": 0, "max": 500, "step": 0.1, "unit": "V", "command_topic": f"{MQTT_BASE_TOPIC}/{serial_no}/set/output_voltage"},
+            "Output Current": {"min": 0, "max": rated_current, "step": 0.1, "unit": "A", "command_topic": f"{MQTT_BASE_TOPIC}/{serial_no}/set/current"},
+            "Altitude": {"min": 1000, "max": 5000, "step": 100, "unit": "m", "command_topic": f"{MQTT_BASE_TOPIC}/{serial_no}/set/altitude"}
         }
 
         # Publish discovery messages for settable parameters
         for param, details in settable_parameters.items():
             discovery_payload = {
                 "name": param,
-                "unique_id": f"uxr_{param.replace(' ', '_').lower()}",
+                "unique_id": f"uxr_{serial_no}_{param.replace(' ', '_').lower()}",
                 "command_topic": details["command_topic"],
                 "min": details["min"],
                 "max": details["max"],
@@ -168,110 +192,114 @@ def ha_discovery():
                 "availability_topic": availability_topic,
                 "device": device
             }
-            discovery_topic = f"{MQTT_HA_DISCOVERY_TOPIC}/number/uxr/{param.replace(' ', '_').lower()}/config"
+            discovery_topic = f"{MQTT_HA_DISCOVERY_TOPIC}/uxr/{serial_no}/{param.replace(' ', '_').lower()}/config"
             client.publish(discovery_topic, json.dumps(discovery_payload), retain=True)
 
         client.publish(availability_topic, "online", retain=True)
 
 # Main loop to continuously read parameters
 try:
-    ha_discovery()
+    for address in uxr_modules:
+        ha_discovery(address)
     while True:
-        # Use lock to ensure thread safety for each sensor reading and publishing
-        with lock:
-            voltage = module.get_module_voltage(address, group)
-            if voltage is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/module_voltage", voltage, retain=True)
-        time.sleep(0.2)
+        for address in uxr_modules:
+            uxr_module = uxr_modules[address]
+            serial_no = uxr_module['serial_no']
+            # Use lock to ensure thread safety for each sensor reading and publishing
+            with lock:
+                voltage = module.get_module_voltage(address, group)
+                if voltage is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/module_voltage", voltage, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            current = module.get_module_current(address, group)
-            if current is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/module_current", current, retain=True)
-        time.sleep(0.2)
+            with lock:
+                current = module.get_module_current(address, group)
+                if current is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/module_current", current, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            current_limit = module.get_module_current_limit(address, group)
-            if current_limit is not None:
-                current_limit = round(current_limit * rated_current, 2)
-                client.publish(f"{MQTT_BASE_TOPIC}/current_limit", current_limit, retain=True)
-                print("Current limit get: {}%".format(current_limit))
-        time.sleep(0.2)
+            with lock:
+                current_limit = module.get_module_current_limit(address, group)
+                if current_limit is not None:
+                    current_limit = round(current_limit * rated_current, 2)
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/current_limit", current_limit, retain=True)
+                    print("Current limit get: {}%".format(current_limit))
+            time.sleep(0.1)
 
-        with lock:
-            temp_dc_board = module.get_temperature_dc_board(address, group)
-            if temp_dc_board is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/temperature_of_dc_board", temp_dc_board, retain=True)
-        time.sleep(0.2)
+            with lock:
+                temp_dc_board = module.get_temperature_dc_board(address, group)
+                if temp_dc_board is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/temperature_of_dc_board", temp_dc_board, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            input_voltage = module.get_input_phase_voltage(address, group)
-            if input_voltage is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/input_phase_voltage", input_voltage, retain=True)
-        time.sleep(0.2)
+            with lock:
+                input_voltage = module.get_input_phase_voltage(address, group)
+                if input_voltage is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/input_phase_voltage", input_voltage, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            pfc0_voltage = module.get_pfc0_voltage(address, group)
-            if pfc0_voltage is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/pfc0_voltage", pfc0_voltage, retain=True)
-        time.sleep(0.2)
+            with lock:
+                pfc0_voltage = module.get_pfc0_voltage(address, group)
+                if pfc0_voltage is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/pfc0_voltage", pfc0_voltage, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            pfc1_voltage = module.get_pfc1_voltage(address, group)
-            if pfc1_voltage is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/pfc1_voltage", pfc1_voltage, retain=True)
-        time.sleep(0.2)
+            with lock:
+                pfc1_voltage = module.get_pfc1_voltage(address, group)
+                if pfc1_voltage is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/pfc1_voltage", pfc1_voltage, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            panel_temp = module.get_panel_board_temperature(address, group)
-            if panel_temp is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/panel_board_temperature", panel_temp, retain=True)
-        time.sleep(0.2)
+            with lock:
+                panel_temp = module.get_panel_board_temperature(address, group)
+                if panel_temp is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/panel_board_temperature", panel_temp, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            voltage_phase_a = module.get_voltage_phase_a(address, group)
-            if voltage_phase_a is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/voltage_phase_a", voltage_phase_a, retain=True)
-        time.sleep(0.2)
+            with lock:
+                voltage_phase_a = module.get_voltage_phase_a(address, group)
+                if voltage_phase_a is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/voltage_phase_a", voltage_phase_a, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            voltage_phase_b = module.get_voltage_phase_b(address, group)
-            if voltage_phase_b is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/voltage_phase_b", voltage_phase_b, retain=True)
-        time.sleep(0.2)
+            with lock:
+                voltage_phase_b = module.get_voltage_phase_b(address, group)
+                if voltage_phase_b is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/voltage_phase_b", voltage_phase_b, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            voltage_phase_c = module.get_voltage_phase_c(address, group)
-            if voltage_phase_c is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/voltage_phase_c", voltage_phase_c, retain=True)
-        time.sleep(0.2)
+            with lock:
+                voltage_phase_c = module.get_voltage_phase_c(address, group)
+                if voltage_phase_c is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/voltage_phase_c", voltage_phase_c, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            temp_pfc_board = module.get_temperature_pfc_board(address, group)
-            if temp_pfc_board is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/temperature_of_pfc_board", temp_pfc_board, retain=True)
-        time.sleep(0.2)
+            with lock:
+                temp_pfc_board = module.get_temperature_pfc_board(address, group)
+                if temp_pfc_board is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/temperature_of_pfc_board", temp_pfc_board, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            input_power = module.get_input_power(address, group)
-            if input_power is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/input_power", input_power, retain=True)
-        time.sleep(0.2)
+            with lock:
+                input_power = module.get_input_power(address, group)
+                if input_power is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/input_power", input_power, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            altitude_value = module.get_current_altitude_value(address, group)
-            if altitude_value is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/current_altitude", altitude_value, retain=True)
-        time.sleep(0.2)
+            with lock:
+                altitude_value = module.get_current_altitude_value(address, group)
+                if altitude_value is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/current_altitude", altitude_value, retain=True)
+            time.sleep(0.1)
 
-        with lock:
-            input_mode = module.get_input_working_mode(address, group)
-            if input_mode is not None:
-                client.publish(f"{MQTT_BASE_TOPIC}/input_working_mode", input_mode, retain=True)
-        time.sleep(0.2)
+            with lock:
+                input_mode = module.get_input_working_mode(address, group)
+                if input_mode is not None:
+                    client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/input_working_mode", input_mode, retain=True)
+            time.sleep(0.1)
 
-        client.publish(f"{MQTT_BASE_TOPIC}/rated_current", rated_current, retain=True)
-        client.publish(f"{MQTT_BASE_TOPIC}/rated_power", rated_power, retain=True)
+            client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/rated_current", rated_current, retain=True)
+            client.publish(f"{MQTT_BASE_TOPIC}/{serial_no}/rated_power", rated_power, retain=True)
 
 except KeyboardInterrupt:
     print("Stopping script...")
