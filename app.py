e2dddc2a2573180d78f188ee68ce2b6e62e939b1
Heinrich <heinrich.delange@gmail.com>
2024-11-14 13:02:38 +0200

    Added default voltage

--- app.py
+++ app.py
@@ -66,9 +66,9 @@ for address in module_address_list:
         "serial_no": serial_no
     }
     time.sleep(read_delay)
-    module.set_current_limit(default_current_limit)
+    module.set_current_limit(default_current_limit, address, group)
     time.sleep(read_delay)
-    module.set_output_voltage(default_voltage)
+    module.set_output_voltage(default_voltage, address, group)
     print(f"Address: {address} ")
     print(f"Rated Output Power: {rated_power} W")
     print(f"Rated Output Current: {rated_current} A")
