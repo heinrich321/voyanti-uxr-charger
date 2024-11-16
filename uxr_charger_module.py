90676e846cf64a82fa38febc138f3b5e0201725b
Heinrich de lange <heinrichdelange@Heinrichs-MacBook-Pro-2.local>
2024-11-16 10:44:24 +0200

    NA

--- uxr_charger_module.py
+++ uxr_charger_module.py
@@ -79,6 +79,7 @@ class UXRChargerModule:
 
         # Construct the data payload
         data = [0x03, 0x00, 0x00, register] + value_bytes
+        print(data)
         # Generate the CAN arbitration ID
         arbitration_id = self.generate_can_arbitration_id(self.protno, 1, address, self.source_address, group)
         # Send the frame
